b6f40a7ef4c3063ac752701c00a038010176d003
Kevin Harrison <kharrison7@elon.edu>
2017-08-15 22:39:00 -0400

    This adds bodyParser.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 console.log("App is running");
@@ -13,6 +15,8 @@ app.set('views', __dirname + '/views')
 
 //Listening on root
 app.get('/todo/', function (req, res) {
+  console.log(req.additions); //This will print the array of values.
+
   // TODO write your code here
   // res.send('Something.');
   // res.sendFile(path.join(__dirname + '/views/todo.html'));
@@ -22,7 +26,6 @@ app.get('/todo/', function (req, res) {
 
 
   res.render('todo.mustache', { name: 'A Todo List:',
-                                todoList: 'Learn Node Basics',
                                 text1: 'Learn Node Basics',
                                 text2: 'Learn Express Basics',
                                 text3: 'Learn Mustache',
@@ -39,13 +42,26 @@ app.get('/todo/', function (req, res) {
                                   "strike": function () {
                                     return function (text, render) {
                                       return "<s>" + render(text) + "</s>";
-                                    }
+                                    };
                                   }
 
 
-
  });
+
 });
+
+let additions = [];
+app.use(bodyParser.urlencoded({ extended: true }));
+console.log("Additions Array: " + additions);
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+};
+
+
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
